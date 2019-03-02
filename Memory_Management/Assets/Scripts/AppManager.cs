c30ebff3679d1821078a2de6a564d1470f18546b
unknown <kevin.butikofer@he-arc.ch>
2019-03-02 11:24:53 +0100

    create room, add character following the path while holding dataBloc, add anti-aliasing, change rendering in deffered mode for dynamic light

---
+++ Memory_Management/Assets/Scripts/AppManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppManager : MonoBehaviour
+{
+    private List<GameObject> progs = new List<GameObject>();
+    public GameObject prefabProg;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            Color color = Random.ColorHSV();
+            GameObject prog = Instantiate(prefabProg);
+            prog.transform.position = new Vector3(8, 1.5f, -30);
+            prog.gameObject.transform.Find("Character").Find("CharacterModel").GetComponent<Renderer>().material.color = color;
+            progs.Add(prog);
+        }
+    }
+}
---
+++ Memory_Management/Assets/Scripts/Bloc.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloc : MonoBehaviour
+{
+    public GameObject prefabDataCube;
+    public RenderTexture texture;
+
+    public Color color;
+    public GameObject dataCube;
+    public int size = 1;
+    public int posInQueue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int r = Random.Range(10000, 999999999);
+        string cubeText = r.ToString();
+        dataCube = Instantiate(prefabDataCube, transform);
+        dataCube.transform.localScale.Set(size, 1, 1);
+        dataCube.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + 1.2f);
+        Color c = gameObject.transform.Find("Character").Find("CharacterModel").GetComponent<Renderer>().material.color;
+        dataCube.GetComponent<Renderer>().material.color = c;
+
+        foreach(Transform t in dataCube.transform)
+        {
+            t.gameObject.GetComponent<TextMesh>().text = cubeText;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ReleaseBloc()
+    {
+        dataCube.GetComponent<Rigidbody>().useGravity = true;
+        dataCube.transform.parent = null;
+    }
+}
